b8db3f13a29ff88507307654ca7d21d9d9715550
Nidhi <nidhigupta02176@gmail.com>
2014-08-20 15:38:15 -0400

    submission to inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# makeCacheMatrix returns a list of object methods(set,get,setinverse,getinverse).
+# it takes input a square matrix that needs to be inversed by the cacheSolve function
+makeCacheMatrix <- function(my_matrix=matrix()){
+  i <- NULL
+  set<-function(new_matrix){
+    my_matrix <<- new_matrix
+    i <<- NULL
+  }
+  get<-function() my_matrix
+  setinverse<-function(inverse) i <<- inverse
+  getinverse<-function() i
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
+#cacheSolve matrix checks to see if the inverse of the matrix already exists.
+#If the inverse is already cached , its returned by cacheSolve function.
+#If the inverse of the matrix does not exist(was not cached), its calculated 
+# first,cached and returned by the function
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matrix_to_be_inversed){
+  i <- matrix_to_be_inversed$getinverse()
+  if(!is.null(i)){
+    print("Inverse is already cached")
+    return(i)
+  }
+  data <- matrix_to_be_inversed$get()
+  i<-solve(data)
+  matrix_to_be_inversed$setinverse(i)
+  i
 }
