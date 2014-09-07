d4d4ebd70cd0573313dc10066fcfddb51edd6fae
Nidhi <nidhigupta02176@gmail.com>
2014-09-06 21:43:51 -0400

    Updated assignment two solution

--- cachematrix.R
+++ cachematrix.R
@@ -7,30 +7,33 @@
 ## it takes input a square matrix that needs to be inversed by the cacheSolve function
 
 makeCacheMatrix <- function(my_matrix=matrix()){
-  i <- NULL
-  set<-function(new_matrix){
-    my_matrix <<- new_matrix
-    i <<- NULL
-  }
-  get<-function() my_matrix
-  setinverse<-function(inverse) i <<- inverse
-  getinverse<-function() i
-  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+      i <- NULL
+      set<-function(new_matrix){
+       
+           my_matrix <<- new_matrix
+           i <<- NULL
+      }
+     
+      get<-function() my_matrix    
+      setinverse<-function(inverse) i <<- inverse
+      getinverse<-function() i
+      list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
+
 #cacheSolve matrix checks to see if the inverse of the matrix already exists.
 #If the inverse is already cached , its returned by cacheSolve function.
 #If the inverse of the matrix does not exist(was not cached), its calculated 
 # first,cached and returned by the function
 
 cacheSolve <- function(matrix_to_be_inversed){
-  i <- matrix_to_be_inversed$getinverse()
-  if(!is.null(i)){
-    print("Inverse is already cached")
-    return(i)
-  }
-  data <- matrix_to_be_inversed$get()
-  i<-solve(data)
-  matrix_to_be_inversed$setinverse(i)
-  i
-
+      i <- matrix_to_be_inversed$getinverse()
+      if(!is.null(i)){
+        
+           print("Inverse is already cached")
+           return(i)
+      }
+      data <- matrix_to_be_inversed$get()
+      i<-solve(data)
+      matrix_to_be_inversed$setinverse(i)
+      i
 }
